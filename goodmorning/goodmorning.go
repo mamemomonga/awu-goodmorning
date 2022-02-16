d21b2c7dd7043d2b0f46b903b212b99774d1e1f6
mamemomonga <mamemomonga@gmail.com>
2022-02-16 20:53:27 +0900

    現在の時刻を修正

--- goodmorning/goodmorning.go
+++ goodmorning/goodmorning.go
@@ -32,7 +32,7 @@ func (t *GoodMorning) Print(pathCode string) {
 	//1行目
 	fmt.Println("今日は" + di.Date + "です。" + "今年は残り" + di.RemainDays + "日です")
 	//2行目
-	fmt.Println("現在の時刻は" + di.RemainDays + "です。")
+	fmt.Println("現在の時刻は" + di.Time + "です。")
 	//区切り線
 	fmt.Println(fmtHR)
 
